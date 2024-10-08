d4a53c5df212be829acc8d4e56eb5b9bff775dac
Hannah Arnett <hannah.arnett@telia.no>
2024-10-08 17:04:51 +0200

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*/.{js, ts, jsx, tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "rgba(0, 0, 0, 0.2)",
+        shadow: "rgba(0, 0, 0, 0.1)",
+        elements: "hsl(0, 0%, 100%)",
+        text: "hsl(200, 15%, 8%)",
+        textInput: "hsl(0, 0%, 52%)",
+        background: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
